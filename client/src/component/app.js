cdd33b9df1d7395e5718b9d6434f38ad17d2b0d4
HiDoYa <hiroyagojo@gmail.com>
2019-03-30 22:59:42 -0700

    Resolved cors

--- client/src/component/app.js
+++ client/src/component/app.js
@@ -4,17 +4,28 @@ import ItemsGrid from './itemsgrid'
 import SortMenu from './sortmenu'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            apiResponse: "",
+        }
+    }
+
     addToCart() {
-        alert(1);
+        // TODO
+        alert(2);
     }
 
     componentDidMount() {
-        
+        fetch("http://localhost:9000/")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
     }
 
     render() {
         return (
             <React.Fragment>
+                <p>{this.state.apiResponse}</p>
                 <CartMenu />
                 <div class="container">
                     <h3 class="display-4 text-center pt-4 mb-4">HiDoYa's Groceries</h3>

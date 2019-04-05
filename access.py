52644956cc90657a8acb92c354d8c18dd2cd7543
HiDoYa <hiroyagojo@gmail.com>
2019-04-05 16:34:09 -0700

    worked on python and mysql

--- access.py
+++ access.py
@@ -6,5 +6,19 @@ db = mysql.connector.connect(
     user="root",
     passwd=sys.argv[1]
 )
+cursor = db.cursor()
+query = ("USE test")
+cursor.execute(query)
+
+query = ("SELECT * FROM pet")
+cursor.execute(query)
+
+for (first_name) in cursor:
+    print(first_name)
 
 print(db)
+# print(cursor.fetchall())
+
+cursor.close()
+db.close()
+

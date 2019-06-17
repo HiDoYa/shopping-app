2da0092cfc50a51ce65bdd98807ed3c90505b8a2
HiDoYa <hiroyagojo@gmail.com>
2019-06-16 22:54:13 -0700

    Added data to send

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var dirname = "/Users/hidoya/Code/shopping-app/api"
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('asdf')
+  res.sendFile(dirname + "/dataSend.json")
 });
 
 module.exports = router;
